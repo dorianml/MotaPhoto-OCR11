c264168bff05c97f0da2da60eaf9bb3396c1b7dc
Dorian <80327420+dorianml@users.noreply.github.com>
2023-12-06 19:22:02 +0100

    update rsponsive

--- header.php
+++ header.php
@@ -3,7 +3,12 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta name="viewport" content="width=device-width" />
+    <!-- <meta name="viewport" content="width=device-width" /> -->
+    <meta name="viewport"  content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0">
+   
+
+
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1" /> -->
     <?php wp_head(); ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
